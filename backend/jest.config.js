4c97bd1f961dda826e9b8aebffefafcee73cdc98
Surya <captianjack2510@gmail.com>
2026-07-08 13:00:05 +0530

    update this files

---
+++ backend/jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  testTimeout: 120000,
+  detectOpenHandles: true,
+  forceExit: true,
+};
