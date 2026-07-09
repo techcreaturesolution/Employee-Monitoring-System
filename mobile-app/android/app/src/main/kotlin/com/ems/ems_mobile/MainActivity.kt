2e01186310e6177bf686667d9084a2df2e4f5080
Surya <captianjack2510@gmail.com>
2026-07-09 10:47:34 +0530

    del : remove this all files

--- mobile-app/android/app/src/main/kotlin/com/ems/ems_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ems.ems_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile-app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:ems_mobile/main.dart';
-
-void main() {
-  testWidgets('App loads', (WidgetTester tester) async {
-    await tester.pumpWidget(const EMSApp());
-    expect(find.text('Employee Monitor'), findsOneWidget);
-  });
-}
